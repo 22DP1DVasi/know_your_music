c24b9da30a30e71a7bcc9c894552dfd754ac145d
danila vasiljevs <dv00710@rvt.lv>
2025-03-19 12:30:11 +0200

    created releases and tracks tables

---
+++ knowm/database/migrations/2025_03_19_102139_create_lyrics_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lyrics', function (Blueprint $table) {
+            $table->id();
+            $table->text('lyrics')->nullable();
+            $table->enum('status', ['verified', 'requires verification'])->default('requires verification');
+            $table->unsignedBigInteger('track_id')->unique();
+            $table->unsignedBigInteger('last_updated_by_user')->nullable(); // nullable because of set null restriction
+            $table->timestamps();
+
+            // Define foreign keys
+            $table->foreign('track_id')
+                ->references('id')
+                ->on('tracks')
+                ->onDelete('cascade'); // cascade delete
+
+            $table->foreign('last_updated_by_user')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null'); // set null on delete
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lyrics');
+    }
+};
