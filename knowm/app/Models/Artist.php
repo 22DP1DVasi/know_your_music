92ad5afaff6d0d95389dfdcfcdc2be2d59254ba9
danila vasiljevs <dv00710@rvt.lv>
2025-04-02 22:15:09 +0300

    + Genre.php model

---
+++ knowm/app/Models/Artist.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Artist extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'biography',
+        'formed_year',
+        'disbanded_year',
+        'image',
+        'solo_or_band'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'formed_year' => 'integer',
+        'disbanded_year' => 'integer',
+    ];
+
+    /**
+     * Get all genres associated with this artist.
+     */
+    public function genres(): BelongsToMany
+    {
+        return $this->belongsToMany(Genre::class, 'artist_genre')
+            ->withTimestamps();
+    }
+
+    /**
+     * Get all releases by this artist.
+     */
+    public function releases(): HasMany
+    {
+        return $this->hasMany(Release::class);
+    }
+
+    /**
+     * Get all tracks by this artist.
+     */
+    public function tracks(): HasMany
+    {
+        return $this->hasMany(Track::class);
+    }
+
+    /**
+     * Scope for active artists (not disbanded).
+     */
+    public function scopeActive($query)
+    {
+        return $query->whereNull('disbanded_year');
+    }
+
+    /**
+     * Scope for bands only.
+     */
+    public function scopeBands($query)
+    {
+        return $query->where('solo_or_band', 'band');
+    }
+
+    /**
+     * Get the artist's active years.
+     */
+    public function getActiveYearsAttribute(): ?string
+    {
+        if (!$this->formed_year) return null;
+
+        return $this->disbanded_year
+            ? "{$this->formed_year} - {$this->disbanded_year}"
+            : "{$this->formed_year} - Present";
+    }
+
+    /**
+     * Get the image URL (accessor).
+     */
+    public function getImageUrlAttribute(): ?string
+    {
+        return $this->image ? asset('storage/artists/'.$this->image) : null;
+    }
+}
