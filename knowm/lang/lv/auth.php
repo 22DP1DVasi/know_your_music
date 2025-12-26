99dcfb9ba42280e25db677f3fc31c3a1ea6b8393
danila vasiljevs <dv00710@rvt.lv>
2025-12-26 11:48:30 +0200

    lang lv files

---
+++ knowm/lang/lv/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Autentifikācijas valodu rindas
+    |--------------------------------------------------------------------------
+    |
+    | Autentifikācijas laikā dažādiem ziņojumiem, kas mums ir jārāda lietotājam,
+    | tiek izmantotas tālāk norādītās valodu rindas. Jūs varat brīvi modificēt
+    | šīs valodu rindas atbilstoši savas lietojumprogrammas prasībām.
+    |
+    */
+
+    'failed' => 'Šie akreditācijas dati neatbilst mūsu ierakstiem.',
+    'password' => 'Norādītā parole nav pareiza.',
+    'throttle' => 'Pārāk daudz pieteikšanās mēģinājumu. Lūdzu, mēģiniet vēlreiz pēc :seconds sekundēm.',
+
+];
